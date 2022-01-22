164132f0f0b2327944e8347b3d62617fe297cfd4
Pankaj kumar <pk497243@gmail.com>
2022-01-22 09:52:22 +0530

    init

--- src/db/models/register.js
+++ src/db/models/register.js
@@ -12,7 +12,8 @@ const empSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique : true
     },
     gender: {
         type: String,
